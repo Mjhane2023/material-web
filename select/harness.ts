ba21cb93b38832fe17a0b186acfed36ee506378c
Material Web Team <material-web-copybara@google.com>
2023-03-22 16:20:59 -0700

    test(select): implement screenshot tests for select
    PiperOrigin-RevId: 518701964

---
+++ select/harness.ts
@@ -0,0 +1,61 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {FieldHarness} from '../field/harness.js';
+import {Field} from '../field/lib/field.js';
+import {Harness} from '../testing/harness.js';
+
+import {Select} from './lib/select.js';
+import {SelectOptionHarness} from './lib/selectoption/harness.js';
+
+class SelectFieldHardness extends FieldHarness {
+  getInteractiveElementPublic() {
+    return this.getInteractiveElement();
+  }
+}
+
+/**
+ * Test harness for menu.
+ */
+export class SelectHarness extends Harness<Select> {
+  protected getField() {
+    return this.element.renderRoot.querySelector('.field') as Field;
+  }
+  /**
+   * Shows the menu and returns the first list item element.
+   */
+  protected override async getInteractiveElement() {
+    await this.element.updateComplete;
+    return this.getField();
+  }
+
+  override async startHover() {
+    const field = await this.getField();
+    const element = await (new SelectFieldHardness(field)).getInteractiveElementPublic();
+    this.simulateStartHover(element);
+  }
+
+  /** @return ListItem harnesses for the menu's items. */
+  getItems() {
+    return this.element.options.map((item) => new SelectOptionHarness(item));
+  }
+
+  async click(quick = true) {
+    this.element.quick = quick;
+    await this.element.updateComplete;
+    const field = await this.getField();
+    field.click();
+  }
+
+  async clickOption(index: number) {
+    const menu = this.element.renderRoot.querySelector('md-menu')!;
+    if (!menu.open) {
+      console.warn(
+          'Internal menu is not open. Try calling SelectHarness.prototype.click()');
+    }
+    this.getItems()[index].element.click();
+  }
+}
